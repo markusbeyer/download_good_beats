2aeefa0539fac44d538a43fdbefe18f08606fe9d
Markus Beyer <markus.beyer10969@web.de>
2022-05-06 20:55:59 +0200

    changed get_detailed_info trying to get more accurate data from the downloaded files and folders

--- get_detailed_info.py
+++ get_detailed_info.py
@@ -1,5 +1,6 @@
 import os, pathlib
 from art import *
+from datetime import datetime
 
 #list dirs + timestamp (OPT: list size and log data for later comparison)
 #list files + timestamp + highlight most recent changes with colors
@@ -12,16 +13,27 @@ for x, y, z in os.walk(os.getcwd()):
         continue
     folder = str(os.path.basename(x))
     tprint(folder, font="cyber")
-    print(str(pathlib.Path.stat(pathlib.Path(x)))+")")
+    print("Last accessed: "+datetime.fromtimestamp(os.path.getatime(x)).strftime("%A, %B %d, %Y %I:%M:%S"))
+    print("Last modified: "+datetime.fromtimestamp(os.path.getmtime(x)).strftime("%A, %B %d, %Y %I:%M:%S"))
+    print("Created      : "+datetime.fromtimestamp(os.path.getctime(x)).strftime("%A, %B %d, %Y %I:%M:%S"))
+    print("Folder Size  : "+str(os.path.getsize(x))+" Bytes")
     print("-Folders in "+folder+":")
     if not y:
         print(" |NO FOLDERS IN "+folder+"!")
     else:
         for i in y:
             print(" |"+i)
+            print("Last accessed: "+datetime.fromtimestamp(os.path.getatime(i)).strftime("%A, %B %d, %Y %I:%M:%S"))
+            print("Last modified: "+datetime.fromtimestamp(os.path.getmtime(i)).strftime("%A, %B %d, %Y %I:%M:%S"))
+            print("Created      : "+datetime.fromtimestamp(os.path.getctime(i)).strftime("%A, %B %d, %Y %I:%M:%S"))
+            print("Folder Size  : "+str(os.path.getsize(i))+" Bytes")
     print("-Files   in "+folder+":")
     if not z:
         print(" |-NO FILES IN "+folder+"!")
     else:
         for i in z:
             print(" |-"+i)
+            print("Last accessed: "+datetime.fromtimestamp(os.path.getatime(os.path.abspath(i))).strftime("%A, %B %d, %Y %I:%M:%S"))
+            print("Last modified: "+datetime.fromtimestamp(os.path.getmtime(os.path.abspath(i))).strftime("%A, %B %d, %Y %I:%M:%S"))
+            print("Created      : "+datetime.fromtimestamp(os.path.getctime(os.path.abspath(i))).strftime("%A, %B %d, %Y %I:%M:%S"))
+            print("File   Size  : "+str(os.path.getsize(i))+" Bytes")
