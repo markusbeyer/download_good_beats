3d150099712559b3cea7f4f06c1449b05a1508ea
Markus Beyer <markus.beyer10969@web.de>
2022-05-07 17:29:31 +0200

    trying to avoid changing user name but still getting info of each downloaded file, relpath didnt even work

--- get_detailed_info.py
+++ get_detailed_info.py
@@ -1,8 +1,8 @@
-import os, re
+import os, re, pathlib
 from art import *
 from datetime import datetime
 
-#list dirs + timestamp (OPT: list size and log data for later comparison)
+#list dirs  + timestamp (OPT: list size and log data for later comparison)
 #list files + timestamp + highlight most recent changes with colors
 #highlight new files
 
@@ -14,7 +14,7 @@ for x, y, z in os.walk(os.getcwd()):
     folder = str(os.path.basename(x))
     tprint(folder, font="cyber")
     print("Last accessed: "+datetime.fromtimestamp(os.path.getatime(x)).strftime("%A, %B %d, %Y %I:%M:%S"))
-    print("Last modified: "+datetime.fromtimestamp(os.path.getmtime(x)).strftime("%A, %B %d, %Y %I:%M:%S"))
+    print("Last modified: "+datetime.fromtimestamp(os.path.getmtime(x)).strftime("%A, %B %d, %Y %I:%M:%S")) # MAIN FOLDER
     print("Created      : "+datetime.fromtimestamp(os.path.getctime(x)).strftime("%A, %B %d, %Y %I:%M:%S"))
     print("Folder Size  : "+str(os.path.getsize(x))+" Bytes")
     print("-Folders in "+folder+":")
@@ -24,7 +24,7 @@ for x, y, z in os.walk(os.getcwd()):
         for i in y:
             print(" |"+i)
             print("Last accessed: "+datetime.fromtimestamp(os.path.getatime(i)).strftime("%A, %B %d, %Y %I:%M:%S"))
-            print("Last modified: "+datetime.fromtimestamp(os.path.getmtime(i)).strftime("%A, %B %d, %Y %I:%M:%S"))
+            print("Last modified: "+datetime.fromtimestamp(os.path.getmtime(i)).strftime("%A, %B %d, %Y %I:%M:%S")) # SUB FOLDERS
             print("Created      : "+datetime.fromtimestamp(os.path.getctime(i)).strftime("%A, %B %d, %Y %I:%M:%S"))
             print("Folder Size  : "+str(os.path.getsize(i))+" Bytes")
     print("-Files   in "+folder+":")
@@ -33,7 +33,17 @@ for x, y, z in os.walk(os.getcwd()):
     else:
         for i in z:
             print(" |-"+i)
-            print("Last accessed: "+datetime.fromtimestamp(os.path.getatime(os.path.abspath(i))).strftime("%A, %B %d, %Y %I:%M:%S"))
-            print("Last modified: "+datetime.fromtimestamp(os.path.getmtime(os.path.abspath(i))).strftime("%A, %B %d, %Y %I:%M:%S"))
-            print("Created      : "+datetime.fromtimestamp(os.path.getctime(os.path.abspath(i))).strftime("%A, %B %d, %Y %I:%M:%S"))
-            print("File   Size  : "+str(os.path.getsize(i))+" Bytes")
+            print(pathlib.Path.stat(str(i)))
+            #file = str(os.path.abspath(i))
+            #file = re.sub(r'^.*?GDrive', 'GDrive', file)
+            #file = file.replace("GDrive_Updater","")
+            #print(file)
+            #print(str(pathlib.Path.stat(pathlib.Path(os.path.relpath(i))))+")")
+            #os.path.getatime(os.path.abspath(file))
+            #print("done")
+            #print("Last accessed: "+datetime.fromtimestamp(os.path.getatime(os.path.abspath(i))).strftime("%A, %B %d, %Y %I:%M:%S"))
+            #print("Last modified: "+datetime.fromtimestamp(os.path.getmtime(os.path.abspath(i))).strftime("%A, %B %d, %Y %I:%M:%S")) # FILES
+            #print("Created      : "+datetime.fromtimestamp(os.path.getctime(os.path.abspath(i))).strftime("%A, %B %d, %Y %I:%M:%S"))
+            #print("File   Size  : "+str(os.path.getsize(i))+" Bytes")  
+            #
+            #  DOESTN WORK DUE TO WHITE SPACE IN MY USERNAME
