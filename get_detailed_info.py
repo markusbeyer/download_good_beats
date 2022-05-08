6f98207f94c9082a3b5adac370f27681eb975aee
Markus Beyer <markus.beyer10969@web.de>
2022-05-08 08:29:11 +0200

    added comments to code for documentation, cant fix this path problem though, posted on stackov

--- get_detailed_info.py
+++ get_detailed_info.py
@@ -2,16 +2,19 @@ import os, re, pathlib
 from art import *
 from datetime import datetime
 
-#list dirs  + timestamp (OPT: list size and log data for later comparison)
-#list files + timestamp + highlight most recent changes with colors
-#highlight new files
+#TO-DO list dirs  + timestamp (OPT: list size and log data for later comparison)
+#TO-DO list files + timestamp + highlight most recent changes with colors
+#TO-DO highlight new files
 
+#walking through downloaded folders
 filenames = next(os.walk(os.getcwd()), (None, None, []))[2]
-lis = []
+
+# x = main folder | y = subfolders | z = files
 for x, y, z in os.walk(os.getcwd()):
+    # avoid getting info about git folder
     if "git" in str(x):
         continue
-    folder = str(os.path.basename(x))
+    folder = str(os.path.basename(x))             #  get main folder name, last accessed time, last modified time, creation time and size in bytes
     tprint(folder, font="cyber")
     print("Last accessed: "+datetime.fromtimestamp(os.path.getatime(x)).strftime("%A, %B %d, %Y %I:%M:%S"))
     print("Last modified: "+datetime.fromtimestamp(os.path.getmtime(x)).strftime("%A, %B %d, %Y %I:%M:%S")) # MAIN FOLDER
@@ -22,7 +25,7 @@ for x, y, z in os.walk(os.getcwd()):
         print(" |NO FOLDERS IN "+folder+"!")
     else:
         for i in y:
-            print(" |"+i)
+            print(" |"+i)                         #  get sub  folder name, last accessed time, last modified time, creation time and size in bytes
             print("Last accessed: "+datetime.fromtimestamp(os.path.getatime(i)).strftime("%A, %B %d, %Y %I:%M:%S"))
             print("Last modified: "+datetime.fromtimestamp(os.path.getmtime(i)).strftime("%A, %B %d, %Y %I:%M:%S")) # SUB FOLDERS
             print("Created      : "+datetime.fromtimestamp(os.path.getctime(i)).strftime("%A, %B %d, %Y %I:%M:%S"))
@@ -40,10 +43,10 @@ for x, y, z in os.walk(os.getcwd()):
             #print(file)
             #print(str(pathlib.Path.stat(pathlib.Path(os.path.relpath(i))))+")")
             #os.path.getatime(os.path.abspath(file))
-            #print("done")
+            #print("done")                         #  get file name, last accessed time, last modified time, creation time and size in bytes
             #print("Last accessed: "+datetime.fromtimestamp(os.path.getatime(os.path.abspath(i))).strftime("%A, %B %d, %Y %I:%M:%S"))
             #print("Last modified: "+datetime.fromtimestamp(os.path.getmtime(os.path.abspath(i))).strftime("%A, %B %d, %Y %I:%M:%S")) # FILES
             #print("Created      : "+datetime.fromtimestamp(os.path.getctime(os.path.abspath(i))).strftime("%A, %B %d, %Y %I:%M:%S"))
             #print("File   Size  : "+str(os.path.getsize(i))+" Bytes")  
             #
-            #  DOESTN WORK DUE TO WHITE SPACE IN MY USERNAME
+            #  DOESNT WORK DUE TO WHITE SPACE IN MY USERNAME
